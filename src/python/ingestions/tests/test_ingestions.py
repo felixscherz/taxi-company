0e428e36126c6a6bca419d257442526ef7faf674
Felix Scherz <felixwscherz@gmail.com>
2025-04-29 21:18:39 +0200

    feat: play with pytest-recording

--- src/python/ingestions/tests/test_ingestions.py
+++ src/python/ingestions/tests/test_ingestions.py
@@ -1,5 +1,20 @@
+import asyncio
+
+import pytest
+import pytest_recording
+from ingestions import DataLoader
+
+
 def test_ingestions():
     assert True
+
+
+@pytest.mark.vcr("taxidata.yaml")
+def test_imports():
+    import ingestions
+
+    loader = DataLoader("./path")
+    asyncio.run(loader.download(2024, 12))
 
 
 def test_httpx():
